431ee4c493fdc5363bdfda8f7a308f5cbccf7a0e
oleg <pozhenskij2@gmail.com>
2020-05-05 21:50:01 +0400

    1.Add a storage and a resume

---
+++ JavaProj/src/com/java/model/Resume.java
@@ -0,0 +1,18 @@
+package com.java.model;
+
+public class Resume {
+
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String toString() {
+        return uuid;
+    }
+}
---
+++ JavaProj/src/com/java/storage/ArrayStorage.java
@@ -0,0 +1,61 @@
+package com.java.storage;
+
+import com.java.model.Resume;
+
+public class ArrayStorage {
+
+    private Resume[] storage = new Resume[5];
+    private int size = 0;
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if(size < storage.length) {
+            int i = 0;
+            while(storage[i] != null) {
+                if(storage[i].toString().equals(r.toString())) {
+                    return;
+                }
+                ++i;
+            }
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid()))
+                return storage[i];
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
+    }
+
+    public Resume[] getAll() {
+        Resume[] result = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = storage[i];
+        }
+        return result;
+    }
+
+    public int size() {
+        return size;
+    }
+
+}
