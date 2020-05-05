3b518da03fe7876cd2983efbf353e014d2483b63
oleg <pozhenskij2@gmail.com>
2020-05-06 02:17:27 +0400

    update Arrays

--- JavaProj/src/com/java/storage/ArrayStorage.java
+++ JavaProj/src/com/java/storage/ArrayStorage.java
@@ -10,7 +10,7 @@ public class ArrayStorage {
     private int size = 0;
 
     public void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage,0, size, null);
         size = 0;
     }
 
