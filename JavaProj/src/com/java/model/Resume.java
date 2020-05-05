49737b744a827884152501589a40eb137873f494
oleg <pozhenskij2@gmail.com>
2020-05-06 02:11:05 +0400

    3.Add equals and hashCode

--- JavaProj/src/com/java/model/Resume.java
+++ JavaProj/src/com/java/model/Resume.java
@@ -1,11 +1,26 @@
 package com.java.model;
 
+import java.util.Objects;
+
 public class Resume {
 
     private String uuid;
 
     public String getUuid() {
         return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
     }
 
     public void setUuid(String uuid) {
