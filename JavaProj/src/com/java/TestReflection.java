3a02bfb215a76f3df8d056cc05d692efb6f90730
OlegPozhenskij <pozhenskij2@gmail.com>
2020-05-08 07:42:10 -0400

    update TestReflection

--- JavaProj/src/com/java/TestReflection.java
+++ JavaProj/src/com/java/TestReflection.java
@@ -2,13 +2,28 @@ package com.java;
 
 import com.java.model.Resume;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TestReflection {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
         Resume resume = new Resume();
         Method method = resume.getClass().getDeclaredMethod("toString");
         System.out.println(method.invoke(resume));
+
+        Class myClass1 = resume.getClass(); // we get class of Resume
+        Class myClass2 = Class.forName("com.java.model.Resume");
+
+        System.out.println(myClass1.getSimpleName());
+        System.out.println(myClass1.getName());
+
+        Field[] fields = myClass1.getFields();
+
+        for (Field f : fields) {
+            System.out.println(f.getName());//only public
+        }
+
+
     }
 }
